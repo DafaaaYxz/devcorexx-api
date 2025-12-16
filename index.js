a3780a074978f4b6475464ed1b8634bf7bbbedc3
Your Name <you@example.com>
2025-12-17 00:19:50 +0700

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const runSetup = require('./utils/initialSetup');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Connect Database
 connectDB().then(() => runSetup());
-
-// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/chat', require('./routes/chatRoutes'));
-app.use('/api/system', require('./routes/systemRoutes')); // Ini yang bikin error kalau filenya gak ada
+app.use('/api/system', require('./routes/systemRoutes'));
 app.use('/api/v1', require('./routes/externalRoutes'));
+app.get('/', (req, res) => {
+  res.json({ 
+    status: 'Online', 
+    system: 'DEVCORE KERNEL V2.1', 
+    uptime: process.uptime() 
+  });
+});
 
 const PORT = 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+EOF
